92dc283480c399c99da7050ed1a5aa151bb9cf17
dmitrysarov <dmvish@yandex.ru>
2021-07-13 12:09:12 +0200

    Update draw_annotation.py

--- draw_annotation/draw_annotation.py
+++ draw_annotation/draw_annotation.py
@@ -60,7 +60,7 @@ def draw_annotation(image: Union[np.ndarray, str, Image.Image], annotation: Unio
                 label_ = str(label)
                 draw.text(annotation, label_, font=fnt, fill=color[0])
             elif isinstance(label, np.ndarray):
-                draw.text(annotation_[:2], str(label[0]), font=fnt, fill=color[0])
+                draw.text(annotation, str(label[0]), font=fnt, fill=color[0])
         if probs is not None:
             draw.text(annotation[::3] - np.array([0, sum(fnt_height)]), f'{probs[0]:.2f}', font=fnt, fill=color[0])
     elif len(annotation.shape) == 2:  # batch of annotation samples
